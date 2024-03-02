8c6d599f68b0128cf5a4f130c6f1e6dd989765fc
hassan-aamer <hassanaamer048@gmail.com>
2024-03-02 21:36:44 +0200

    Create Auth Package

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('auth.login');
 });
 
-Route::get('/{page}', [App\Http\Controllers\AdminController::class, 'index']);
 
 
 
+
+
+Auth::routes();
+
+
+Route::get('/{page}', [App\Http\Controllers\AdminController::class, 'index'])->middleware('auth');
